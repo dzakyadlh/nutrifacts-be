ce0a21246de6c5406d3163e6d9e846291d9779c3
ranggasaputra1 <ranggasaputra10092003@gmail.com>
2023-12-07 11:21:28 +0700

    mengubah database ke database cloud mysql

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 // Koneksi ke Database MySql
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '34.101.221.64',
   user: 'root',
-  password: '',
-  database: 'capstone_project',
+  password: 'capstone-project-CH2-PS588',
+  database: 'capstone-project',
 });
 connection.connect((err) => {
   if (err) {
